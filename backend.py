2c6d067738b8f49b39aa47aa6301dbbd60094230
divyas248 <hidivya24@gmail.com>
2024-12-10 23:47:50 -0500

    Cleaning Up Code and After Using Pylint

--- backend.py
+++ backend.py
@@ -1,150 +1,161 @@
+'''
+This file is used for running the flask application and connecting to different services:
+- SageMaker Inference Endpoint
+- S3
+- Lambda
+- DynamoDB
+'''
+
+import json
+import os
+
+import boto3
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from botocore.exceptions import ClientError
-import boto3
-import os
-import json
 
 app = Flask(__name__)
 
-#Cors - Change this for Prod
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+# Cors - Change this for Prod
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "http://localhost:5173",
+                "https://main.ds37782qp0di8.amplifyapp.com",
+            ]
+        }
+    },
+)
 
 
 # Required AWS credentials
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["AWS_ACCESS_KEY_ID"] = "REMOVED_FOR_SECURITY"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "REMOVED_FOR_SECURITY"
 os.environ["AWS_DEFAULT_REGION"] = "us-east-2"
-# Required resources
+
+# Dynamo DB required configuration
 USER_METADATA_TABLE_NAME = "user-metadata"
-DYNAMODB_RESOURCE = boto3.resource('dynamodb')
+DYNAMODB_RESOURCE = boto3.resource("dynamodb")
+DYNAMODB_TABLE = "user-metadata"
+USER_METADATA_TABLE = DYNAMODB_RESOURCE.Table(USER_METADATA_TABLE_NAME)
+
+# SageMaker Parameters
 SAGEMAKER_RESOURCE = boto3.client(
-    'sagemaker-runtime',
-    aws_access_key_id="",
-    aws_secret_access_key="",
-    region_name='us-east-2'
+    "sagemaker-runtime",
+    aws_access_key_id="REMOVED_FOR_SECURITY",
+    aws_secret_access_key="REMOVED_FOR_SECURITY",
+    region_name="us-east-2",
 )
-USER_METADATA_TABLE = DYNAMODB_RESOURCE.Table(USER_METADATA_TABLE_NAME)  
-S3_CLIENT = boto3.client('s3')
-LAMBDA_CLIENT = boto3.client('lambda')
-LAMBDA_FUNCTION_NAME = "AddChatHistory"
-BUCKET_NAME = 'ece1779-chat-history'
 SAGEMAKER_ENDPOINT_NAME = "huggingface-pytorch-tgi-inference-2024-12-08-15-56-08-806"
 
+# S3 Parameters
 
-def check_dynamo_db_user_name (username: str):
-    response = USER_METADATA_TABLE.get_item(Key={'username': username})
+S3_CLIENT = boto3.client("s3")
+BUCKET_NAME = "ece1779-chat-history"
+
+# Lambda Parameters
+LAMBDA_CLIENT = boto3.client("lambda")
+LAMBDA_FUNCTION_NAME = "AddChatHistory"
+
+
+def check_dynamo_db_user_name(username: str):
+    """
+    Checks if the username exists in the DynamoDB
+    - username: string
+    """
+    response = USER_METADATA_TABLE.get_item(Key={"username": username})
     return response
 
-def receive_last_five_prompts (username, chat_id):
-    key = f"{username}/{chat_id}.json"
-    # Attempt to fetch the object
-    response = S3_CLIENT.get_object(Bucket=BUCKET_NAME, Key=key)
-    file_content = response['Body'].read().decode('utf-8')
-    json_dict = json.loads(file_content)
-    print(json_dict)
-    print(len(json_dict))
-    if len(json_dict)<5:
-        num_prompts =  len(json_dict)
-    else:
-        num_prompts = 5
 
-    recent_prompts = json_dict[-num_prompts:]
-    print(len(recent_prompts))
-    recent_prompts = [hist["prompt"] for hist in recent_prompts]
-
-    return recent_prompts
-
-
-DYNAMODB_TABLE = "user-metadata"
-# curl -X POST -H "Content-Type: application/json" \
-#     -d '{"username":"John", "prompt": "Hello how are you?", "chat_id": 2}' \
-#     "http://127.0.0.1:5000/chatbot_response"
-@app.route('/chatbot_response', methods=['POST'])
+@app.route("/chatbot_response", methods=["POST"])
 def chatbot_response():
-    # Run Model Invocation
+    """
+    Run model invocation by calling SageMaker Inference Endpoint
+    - username: string
+    - chat_id: int 
+    - prompt: string
+    """
     data = request.get_json()
     print(data)
 
-
-    instructions =  """instructions: You are a friendly and empathetic companion. 
-                    Engage in meaningful conversations, respond empathetically to the user's 
-                    feelings and thoughts, and gently decline inappropriate or harmful topics by saying,
-                    'I'm sorry, but I can't assist with that.' 
-                    Keep your responses clear, concise, and supportive. 
-                    Use the following past conversation as context to understand and respond appropriately:
-                """
-    past_five_prompts = receive_last_five_prompts(data['username'], data['chat_id'])
-    # Use the following past conversation as context to understand and respond appropriately: My name is Anubhav I live in India I am 23 years old I study in University of Toronto current_task: Current Task: Respond to this prompt only:'Hi, how are you? Do you remember my name?'
-    instructions = "You are a friendly and empathetic companion. Engage in meaningful conversations, respond empathetically to the user's feelings and thoughts, and gently decline inappropriate or harmful topics by saying. Respond to this prompt only :"
+    instructions = """You are a friendly and empathetic companion.
+    Engage in meaningful conversations, respond empathetically to the user's
+    feelings and thoughts, and gently decline inappropriate or harmful topics. 
+    Respond to this prompt only:
+    """
     payload = {
-        "inputs": instructions + " ".join(past_five_prompts) + "Current Task: Respond to this prompt only:" + data['prompt'],
+        "inputs": instructions + data["prompt"],
         "parameters": {
             "max_new_tokens": 100,
             "temperature": 0.7,
             "top_p": 0.9,
-            "return_full_text": False
-
-        }
+            "return_full_text": False,
+        },
     }
 
     model_response = SAGEMAKER_RESOURCE.invoke_endpoint(
         EndpointName=SAGEMAKER_ENDPOINT_NAME,
-        ContentType='application/json',
-        Body=json.dumps(payload).encode('utf-8')
+        ContentType="application/json",
+        Body=json.dumps(payload).encode("utf-8"),
     )
 
-    model_response = model_response['Body'].read().decode('utf-8')
+    model_response = model_response["Body"].read().decode("utf-8")
     model_response = json.loads(model_response)
     model_reply = model_response[0]["generated_text"][1:]
 
     print(f"Model Response: {model_reply}")
 
-    model_history_entry = {
-        "prompt": data["prompt"],
-        "model_response": model_reply
-    }
+    model_history_entry = {"prompt": data["prompt"], "model_response": model_reply}
 
     payload = {
-        "username": data['username'],
+        "username": data["username"],
         "chat_id": data["chat_id"],
-        "model_history_entry": model_history_entry
+        "model_history_entry": model_history_entry,
     }
     LAMBDA_CLIENT.invoke(
         FunctionName=LAMBDA_FUNCTION_NAME,
-        InvocationType='Event',  # Wait for the response
-        Payload=json.dumps(payload)
+        InvocationType="Event",  # Wait for the response
+        Payload=json.dumps(payload),
     )
 
+    return jsonify({"status_code": 200, "response": model_reply})
 
-    return jsonify({"status_code":200, "response": model_reply})
 
-
-@app.route('/user_authentication', methods=['POST'])
-# curl -X POST -H "Content-Type: application/json" \
-#     -d '{"username":"John","password":"30"}' \
-#     "http://127.0.0.1:5000/user_authentication"
+@app.route("/user_authentication", methods=["POST"])
 def user_authentication():
+    """
+    Checks if the username and password combination exists in the
+    DynamoDB. If yes, then return 200 else return 400
+    - username: string
+    - password: string
+    """
     data = request.get_json()
 
     if not data or not data.get("username") or not data.get("password"):
-        return jsonify({"status_code": 400, "message": "Username and password are required."})
+        return jsonify(
+            {"status_code": 400, "message": "Username and password are required."}
+        )
 
     response = check_dynamo_db_user_name(data["username"])
     if "Item" in response.keys():
-        if response["Item"]['password'] == data['password']:
+        if response["Item"]["password"] == data["password"]:
             return jsonify({"status_code": 200, "message": "Login was successful"})
-        else:
-            return jsonify({"status_code": 400, "message": "Incorrect password. Try again."})
+
+        return jsonify(
+            {"status_code": 400, "message": "Incorrect password. Try again."}
+        )
 
     return jsonify({"status_code": 400, "message": "Username does not exist."})
 
 
-@app.route('/get_chat_history', methods=["POST"])
-# curl -X GET -H "Content-Type: application/json" \
-#     -d '{"username":"John","chat_id": 1}' \
-#     "http://127.0.0.1:5000/get_chat_history"
+@app.route("/get_chat_history", methods=["POST"])
 def get_chat_history():
+    """
+    Retrieves the chat history by connecting with S3 and parsing JSON files
+    - username: string
+    """
     data = request.get_json()
     response = check_dynamo_db_user_name(data["username"])
 
@@ -161,38 +172,35 @@ def get_chat_history():
             try:
                 # Attempt to fetch the object
                 response = S3_CLIENT.get_object(Bucket=BUCKET_NAME, Key=key)
-                file_content = response['Body'].read().decode('utf-8')
+                file_content = response["Body"].read().decode("utf-8")
                 json_dict = json.loads(file_content)
                 print(f"Successfully read JSON from S3: {json_dict}")
                 curr_chat_history = {i: json_dict}
                 chat_history.append(curr_chat_history)
             except ClientError as e:
-                if e.response['Error']['Code'] == 'NoSuchKey':
+                if e.response["Error"]["Code"] == "NoSuchKey":
                     print(f"Key not found: {key}")
-                    continue  # Skip missing keys
-                else:
-                    # Raise unexpected errors
-                    raise
-            except Exception as e:
-                print(f"Unexpected error fetching {key}: {e}")
-                return jsonify({"status_code": 500, "message": "Error fetching chat history"}), 500
+                    continue
+                raise FileNotFoundError(f"Error accessing the S3 file: {str(e)}") from e
 
     return jsonify({"status_code": 200, "chat_history": chat_history})
 
 
-@app.route('/new_chat', methods=["POST"])
-# curl -X POST -H "Content-Type: application/json" \
-#     -d '{"username":"John","password":30}' \                                              
-#     "http://127.0.0.1:5000/new_chat" 
+@app.route("/new_chat", methods=["POST"])
 def new_chat():
+    """
+    Create a new chat by creating an empty JSON file in S3 and incrementing
+    chat_id for the user in DynamoDB
+    - username: string
+    """
     data = request.get_json()
     username = data["username"]
 
     response = USER_METADATA_TABLE.update_item(
-        Key={'username': username},
+        Key={"username": username},
         UpdateExpression="ADD chat_count :increment",
-        ExpressionAttributeValues={':increment': 1},
-        ReturnValues="UPDATED_NEW"
+        ExpressionAttributeValues={":increment": 1},
+        ReturnValues="UPDATED_NEW",
     )
 
     new_chat_id = int(response["Attributes"]["chat_count"])
@@ -205,36 +213,54 @@ def new_chat():
             Bucket=BUCKET_NAME,
             Key=key,
             Body=json.dumps(placeholder),
-            ContentType="application/json"
+            ContentType="application/json",
         )
         print(f"Created placeholder file in S3: {key}")
     except ClientError as e:
         print(f"Failed to create placeholder file in S3: {e}")
-        return jsonify({"status_code": 500, "message": "Failed to initialize new chat"}), 500
+        return (
+            jsonify({"status_code": 500, "message": "Failed to initialize new chat"}),
+            500,
+        )
     return jsonify({"status_code": 200, "chat_id": new_chat_id})
 
 
-@app.route('/user_creation', methods=["POST"])
-# curl -X POST -H "Content-Type: application/json" \
-#     -d '{"username":"John","password":30}' \
-#     "http://127.0.0.1:5000/user_creation"
+@app.route("/user_creation", methods=["POST"])
 def user_creation():
+    """
+    Checks if the provided username is in the DynamoDB table, if not
+    then a new user is created with the provided password. 
+    - username: string
+    - password: string
+    """
     # Read the Dynamo DB
     data = request.get_json()
-    response = check_dynamo_db_user_name (data["username"])
+    response = check_dynamo_db_user_name(data["username"])
 
     if "Item" in response.keys():
-        return jsonify({"status_code": 400, "message": "User profile already exists. Try logging in."})
-
-     
-    USER_METADATA_TABLE.put_item(
-            Item={
-                'username': data["username"],
-                'password': data["password"],
-                'chat_count': 0
-                # Can add any other required metadata
+        return jsonify(
+            {
+                "status_code": 400,
+                "message": "User profile already exists. Try logging in.",
             }
         )
-    return jsonify({"status_code": 200, "message": "Account Creation was successful. Now try logging in."})
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+    USER_METADATA_TABLE.put_item(
+        Item={
+            "username": data["username"],
+            "password": data["password"],
+            "chat_count": 0
+            # Can add any other required metadata
+        }
+    )
+    return jsonify(
+        {
+            "status_code": 200,
+            "message": "Account Creation was successful. Now try logging in.",
+        }
+    )
+
+
+if __name__ == "__main__":
+    # Used to test - local server
+    app.run(host="0.0.0.0", port=5000)
